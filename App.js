89700d452ef021e41b4e2515e4662c2129558b4a
Vamsi Krishna <20816971+petlurivamsi@users.noreply.github.com>
2023-04-05 23:15:48 +0530

    basic react with js

---
+++ App.js
@@ -0,0 +1,30 @@
+/**********
+ *
+ * <div id='parent'>
+ *  <div id='parent'>
+ *          <h1 id='heading'>Hi, I am heading in react</h1>
+ * </div>
+ * </div>
+ *
+ *
+ *
+ *
+ */
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world from React..!"
+);
+
+const headingInDiv = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", { id: "heading" }, "I am heading from div class")
+  )
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(headingInDiv);
